b21cdc2329fe7c8ef65048cc00f06432604f15ab
MaxRBudib <maximilianoromerobudib@yahoo.com.mx>
2023-06-21 03:15:26 -0600

    Finished login except picture

--- react-ts/src/components/Login.tsx
+++ react-ts/src/components/Login.tsx
@@ -5,19 +5,23 @@ export const Login = () => {
         <div className="h-screen w-screen flex">
             <div className=" w-1/2 bg-[#EDF2FB] flex flex-col justify-center items-center">
                 <div className="my-5">ImagenAI</div>
-                <div className="my-5">Bienvenixs!</div>
-                <div>NoMoreHunger El cambio comienza contigo un grano a la vez</div>
+                <div className="font-['Lato'] my-5 text-5xl font-extrabold text-[#071A4A]">Bienvenixs!</div>
+                <div className="font-['Lato'] my-5 text-xl font-extrabold text-['#071A4A']">NoMoreHunger El cambio comienza contigo un grano a la vez</div>
                 <form className="flex flex-col justify-center items-center" >
                     <input type="email" placeholder="Email" className=" my-5 border-b-2 border-black bg-[#EDF2FB]"></input>
                     <input type="email" placeholder="Password" className="my-5 border-b-2 border-black bg-[#EDF2FB]"></input>
                     <div>
                      <input type="checkbox" className="my-5 border-b-2 border-black bg-[#EDF2FB]"></input>
-                     <label >Recuerdame</label>
+                     <label className="mx-3">Recuerdame</label>
                     </div>
                     <button className="my-5 bg-orange-500 rounded-lg px-4 py-2 w-full text-white">Ingresa</button>
                 </form>
             </div> 
-            <div className=" w-1/2 bg-[#CCDBFD]">Aqui en teoria ira la imagen</div> 
+            <div className=" w-1/2 bg-[#CCDBFD]">
+                <div className="flex justify-start">
+                    <img src="..\public\undraw_walking_outside_re_56xo.svg" alt="not_found" />
+                </div>
+            </div> 
         </div>
     </>
   )
--- react-ts/tailwind.config.js
+++ react-ts/tailwind.config.js
@@ -4,6 +4,9 @@ export default {
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
+        fontFamily: {
+            sans: ['Lato', 'sans-serif']
+        },
         extend: {},
     },
     plugins: [],
