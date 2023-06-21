729de36ac8062f83fdb6600f7649af2be2b5d204
MaxRBudib <maximilianoromerobudib@yahoo.com.mx>
2023-06-21 05:56:23 -0600

    Home

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,11 +1,13 @@
 import {Route, Routes} from 'react-router-dom'
 import { Login } from './components/Login'
+import { Home } from './components/Home'
 function App() {
 
   return (
     <>
       <Routes>
         <Route path='login' element={<Login />}></Route>
+        <Route path='home' element={<Home />}></Route>
       </Routes>
     </> 
   )
